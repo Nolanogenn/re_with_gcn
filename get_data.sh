4997d0b97bde4d00ed4187f50c3ec8f1527524a1
Gennaro Nolano <nolanogenn@gmail.com>
2022-05-26 19:07:17 +0200

    first commit

---
+++ get_data.sh
@@ -0,0 +1,14 @@
+echo "downloading trex data..."
+
+#the code downloads the sample by default, you can change this by commenting the following line and uncommenting the one after
+url="https://figshare.com/ndownloader/files/8768701"
+#url=https://figshare.com/ndownloader/files/8760241
+
+mkdir data
+cd data
+wget $url
+unzip {$url:39:7} 
+rm {$ulr:39:7}
+
+cd ..
+echo "done!"
